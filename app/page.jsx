229bf229aeb55bbc03b7a2c2da64f004a9b22b4d
soubhagyacodes <soubhagya613mishra@gmail.com>
2025-02-21 13:27:58 +0530

    Added Initial Section Templates

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Budget from "./components/Budget";
+import Hero from "./components/Hero";
+import Section2 from "./components/Section2";
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <Section2 />
+      <Budget/>
+    </div>
+  );
+}
